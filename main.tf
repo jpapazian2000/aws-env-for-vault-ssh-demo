50bb96512ea6dbbc4fdfba242576030e9320dcfc
jpapazian2000 <jpapazian@hashicorp.com>
2020-12-14 14:45:03 +0100

    add network acl

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_network_acl" "solvay_vpc" {
     protocol   = "-1"
     rule_no    = 200
     action     = "allow"
-    cidr_block = "0.0.0.0/16"
+    cidr_block = "0.0.0.0/0"
     from_port  = 0
     to_port    = 0
   }
@@ -76,7 +76,7 @@ resource "aws_network_acl" "solvay_vpc" {
     protocol   = "-1"
     rule_no    = 100
     action     = "allow"
-    cidr_block = "0.0.0.0/16"
+    cidr_block = "0.0.0.0/0"
     from_port  = 0
     to_port    = 0
   }
