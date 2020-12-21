7f6d071e811037255f3e1754b24631070a34b15a
jpapazian2000 <jpapazian@hashicorp.com>
2020-12-21 12:09:54 +0100

    modified just network config

--- main.tf
+++ main.tf
@@ -154,27 +154,27 @@ resource "aws_security_group" "allow_ssh_from_private" {
     }
 }
 
-resource "aws_instance" "ubuntu_public" {
-    ami                         = var.ubuntu_ami
-    instance_type               = var.instance_type
-    key_name                    = aws_key_pair.ubuntu_kp.key_name
-    subnet_id                   = aws_subnet.private.id
-    vpc_security_group_ids      = [
-        aws_security_group.allow_ssh_from_public.id,
-    ]
-    associate_public_ip_address = true
+#resource "aws_instance" "ubuntu_public" {
+#    ami                         = var.ubuntu_ami
+#    instance_type               = var.instance_type
+#    key_name                    = aws_key_pair.ubuntu_kp.key_name
+#    subnet_id                   = aws_subnet.private.id
+#    vpc_security_group_ids      = [
+#        aws_security_group.allow_ssh_from_public.id,
+#    ]
+#    associate_public_ip_address = true
 
-    depends_on      = [aws_internet_gateway.gw]
+#    depends_on      = [aws_internet_gateway.gw]
 
-    tags = {
-        Name        = "jpapazian-solvay-public"
-        owner       = "jpapazian"
-        se-region   = "europe west 3"
-        purpose     = "customer solvay vault ssh demo"
-        ttl         = "8"
-        terraform   = "yes"
-    }
-}
+#    tags = {
+#        Name        = "jpapazian-solvay-public"
+#        owner       = "jpapazian"
+#        se-region   = "europe west 3"
+#        purpose     = "customer solvay vault ssh demo"
+#        ttl         = "8"
+#        terraform   = "yes"
+#    }
+#}
 
 #resource "aws_instance" "ubuntu_private" {
 #    ami                         = var.ubuntu_ami
