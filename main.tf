668ee3149fc79d4995fe829eac0cb50016c86222
jpapazian2000 <jpapazian@hashicorp.com>
2020-12-21 17:45:37 +0100

    modified just network config

--- main.tf
+++ main.tf
@@ -163,7 +163,7 @@ resource "aws_instance" "ubuntu_public" {
     key_name                    = aws_key_pair.ubuntu_kp.key_name
     subnet_id                   = aws_subnet.private.id
     vpc_security_group_ids      = [
-        aws_security_group.allow_ssh_from_public.id,
+        aws_default_security_group.allow_ssh_from_public.id,
     ]
     associate_public_ip_address = true
 
