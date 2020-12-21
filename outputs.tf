c4216bb46001f8a0f7659ab0d1cfb2a828c0bb9c
jpapazian2000 <jpapazian@hashicorp.com>
2020-12-21 12:10:58 +0100

    modified just network config

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "ubuntu_pub-server_name" {
-    value = aws_instance.ubuntu_public.public_dns
-}
+#output "ubuntu_pub-server_name" {
+#    value = aws_instance.ubuntu_public.public_dns
+#}
 
-output "ubuntu_pub-server_public_ip" {
-    value = aws_instance.ubuntu_public.public_ip
-}
+#output "ubuntu_pub-server_public_ip" {
+#    value = aws_instance.ubuntu_public.public_ip
+#}
 
-output "ubuntu_pub-server_private_ip" {
-    value = aws_instance.ubuntu_public.private_ip
-}
+#output "ubuntu_pub-server_private_ip" {
+#    value = aws_instance.ubuntu_public.private_ip
+#}
 
 #output "ubuntu_priv-server_private_ip" {
 #    value = aws_instance.ubuntu_private.private_ip
