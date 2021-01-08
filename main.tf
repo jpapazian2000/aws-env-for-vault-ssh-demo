dda59f6d0b89a4fc2895443103ba12634bfcc562
jpapazian2000 <jpapazian@hashicorp.com>
2021-01-08 10:44:37 +0100

    cert modification for bastion and servers

--- main.tf
+++ main.tf
@@ -196,9 +196,9 @@ resource "aws_instance" "ubuntu_private" {
     key_name                    = aws_key_pair.ubuntu_kp.key_name
     instance_type               = var.instance_type
     subnet_id                   = aws_subnet.private.id
-    vpc_security_group_ids      = [
-        aws_security_group.allow_ssh_from_private.id,
-    ]
+    #vpc_security_group_ids      = [
+    #    aws_security_group.allow_ssh_from_private.id,
+    #]
     associate_public_ip_address = false
     user_data                   = data.template_file.private_user_data.rendered
     depends_on      = [aws_internet_gateway.gw]
